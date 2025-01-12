6804e8511355f94b1c0e5fe9bc9e9b08ae7da629
Saulo Coelho <coelho.saulo@gmail.com>
2025-01-12 14:44:41 -0300

    refract:changef Link to Imperative Function

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,16 @@
 import { View,Text, TouchableOpacity, StyleSheet } from "react-native"
-import { Link } from "expo-router"
+import {router} from 'expo-router'
 
 export default function Index(){
+	function signUp(){
+		router.navigate('/sign-up')
+	}
+
 	return(
 		<View style={styles.container}>
-      <Link href="/sign-up" asChild >
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity style={styles.button} onPress={signUp}>
         	<Text style={styles.new}>Criar Conta</Text>
         </TouchableOpacity>
-      </Link>
     </View>
   )
 }
--- src/app/sign-up.tsx
+++ src/app/sign-up.tsx
@@ -1,15 +1,22 @@
-import { View, StyleSheet } from "react-native"
-import { Link } from "expo-router"
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native"
+import { router } from "expo-router"
 
-export default function Index(){
-    return(
-        <View style={styles.container}>
-            <Link href="/" style={styles.back}>Voltar</Link>
-        </View>
-    )
+export default function Signup(){
+	function back(){
+		router.navigate('/')
+	}
+
+	return(
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button} onPress={back}>
+        <Text style={styles.back}>Voltar</Text>
+      </TouchableOpacity>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {flex:1, justifyContent:"center", alignItems:"center"},
-    back: {fontSize: 16, fontWeight:"bold"},
+  container: {flex:1, justifyContent:"center", alignItems:"center"},
+  back: {fontSize: 16, fontWeight:"bold", color:"white"},
+	button:{backgroundColor:"#000", paddingHorizontal: 32, paddingVertical: 10, borderRadius:10}
 })
